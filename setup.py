137be9955efd3262fbf9ba1a09307851463e0ada
Xuankang Lin <xuankang.lin@gmail.com>
2020-07-23 14:59:48 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import setuptools
+
+curr_dir = Path(__file__).resolve().parent
+with open(curr_dir / 'README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="diffabs",
+    version="0.1",
+    author="Xuankang Lin",
+    author_email="xuankang.lin@gmail.com",
+    description="Differentiable abstract domain implementations for neural network reasoning on PyTorch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/XuankangLin/DiffAbs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
